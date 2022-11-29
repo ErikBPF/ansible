08d7b6b6bb0801a2ce8a253190cffae8406aac30
ErikBPF <erikbogado@gmail.com>
2022-11-29 18:06:32 -0300

    add qtile

---
+++ files/qtile/autostart.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+lxsession &
+xset r rate 300 50 &
+blueman-applet&
+nm-applet &
+pa-applet &
+pulseeffects -w &
+dunst &
+feh --randomize --bg-fill ~/.config/wallpapers/wallpaper.png &
+betterlockscreen -u ~/.config/wallpapers &
+xidlehook --not-when-audio --timer 60 "brightnessctl set 5%" "brightnessctl set 100%" &
+xidlehook --not-when-audio --timer 75 "brightnessctl set 0%" "brightnessctl set 100%" &
+xidlehook --not-when-audio --timer 75 "betterlockscreen -l dim --off 10" "" &
